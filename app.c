c94550a70898c63f9fee4e486e51f58c35b8312a
lamine-f <mouhamedlaminefaye77@gmail.com>
2024-03-27 23:07:26 +0000

    refractor

--- app.c
+++ app.c
@@ -4,24 +4,16 @@
 #include "not_formal.h"
 
 void arithmetic_resolver () {
-    
+
 }
 
 int main() {
 
     while (true)
     {
-
         printf("A toi :");
         parser();
         clear_buffer();
-        /* code */
     }
-    
-
-
-        /* code */
-    
-
     return 0;
 }
