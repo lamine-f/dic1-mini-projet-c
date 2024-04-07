753fc6edacd5115bc828eb1f1983860c0408bcf7
lamine-f <mouhamedlaminefaye77@gmail.com>
2024-04-07 16:32:39 +0000

    update includes set define condition

--- include/syntax_analysis_expression_evaluation.h
+++ include/syntax_analysis_expression_evaluation.h
@@ -1,13 +1,15 @@
+#ifndef SYNTAX_ANALYSIS_EXPRESSION_EVAL_H_INCLUDED
+#define SYNTAX_ANALYSIS_EXPRESSION_EVAL_H_INCLUDED
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
 #include <ctype.h>
 #include "term_struct.h"
 #include "expression_struct.h"
-
 void arithmetic_resolver ();
 void parser ();
 int expression ();
 int term ();
 int factor ();
 int number ();
+#endif
--- include/term_struct.h
+++ include/term_struct.h
@@ -1,15 +1,16 @@
+#ifndef TERM_STRUCT_H_INCLUDED
+#define TERM_STRUCT_H_INCLUDED
 #include <stdio.h>
 #include <stdlib.h>
-
 typedef struct Term_stack {
     int value;
     struct Term_stack *next_term;
     char operator;
 } Term_stack ;
-
 int evaluate_term (Term_stack *term_stack);
 Term_stack *create_term_stack();
 int is_empty_term_stack (Term_stack *term_stack);
 Term_stack *clear_term_stack (Term_stack *term_stack);
 Term_stack *add_term(Term_stack *term_stack, char operator, int value);
 void print_term_stack(Term_stack *term_stack);
+#endif
