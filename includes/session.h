a0cf111f3204fcc5cc82d45a259e919c038c9230
imRYiUK <102487900+imRYiUK@users.noreply.github.com>
2024-04-15 18:20:19 +0000

    Update session.h

--- includes/session.h
+++ includes/session.h
@@ -1,10 +1,15 @@
+// session.h
 #ifndef SESSION_H_INCLUDED
 #define SESSION_H_INCLUDED
 #include <stdbool.h>
 #include <unistd.h>
 #include <sys/wait.h>
-extern bool SESSION;
-void start_session();
-void stop_session();
-void abort_process(int status_code, char* message);
+
+extern bool SESSION; // Déclaration de la variable globale SESSION
+
+// Prototypes des fonctions
+void start_session(); // Démarre une session
+void stop_session(); // Arrête une session
+void abort_process(int status_code, char* message); // Termine un processus avec un code d'état et un message
+
 #endif
