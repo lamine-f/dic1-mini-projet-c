f49407f7e660f0c7bdc218ce344f92b40610beeb
imRYiUK <102487900+imRYiUK@users.noreply.github.com>
2024-04-15 18:24:12 +0000

    Update utils.c

--- src/utils.c
+++ src/utils.c
@@ -1,25 +1,31 @@
+// utils.c
 #include "../includes/utils.h"
 
-char read_character;
+char read_character; // Définition de la variable globale read_character
 
+// Vide le buffer d'entrée
 void clear_buffer () {
     int c = 0;
     while (c != '\n' && c != EOF)
         c = getchar();
 }
 
+// Initialise la lecture des caractères
 void init_read_character() {
     clear_buffer();
 }
 
+// Lit le prochain caractère non-blanc
 void read_next_character () {
     while ( isblank(read_character = getchar()) );
 }
 
+// Affiche un message d'erreur
 void print_error_message (char *prefix, char *message) {
     printf("%s: %s\n", prefix, message);
 }
 
+// Affiche le résultat
 void print_result (int value) {
     printf(
             "la syntaxe de l’expression est correcte"
@@ -28,6 +34,7 @@ void print_result (int value) {
     );
 }
 
+// Convertit un caractère en chiffre
 int char_to_digit (char c) {
     switch (c) {
         case '0':
@@ -55,11 +62,13 @@ int char_to_digit (char c) {
     }
 }
 
+// Calcule une puissance
 int power (int number, int pow_value) {
     if (pow_value == 0) return 1;
     return number*power(number, pow_value-1);
 }
 
+// Convertit une chaîne de caractères en nombre
 int str_to_number (char *str, int len) {
     int result = 0;
     for(int i=0; i<len; i++) {
